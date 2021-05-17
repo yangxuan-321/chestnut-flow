4e8c7249142ba0d51389c012f72c0254a702a079
yangxuan_321 <yangxuan_321@163.com>
2021-05-17 23:39:42 +0800

    修改表结构

---
+++ doc/sql/chestnut_flow_node_instance.sql
@@ -0,0 +1,23 @@
+CREATE TABLE chestnut_flow_node_instance(
+    id bigserial NOT NULL,
+    node_id bigint DEFAULT 0 NOT NULL,
+    flow_instance_id bigint DEFAULT 0 NOT NULL,
+    status smallint DEFAULT 0 NOT NULL,
+    seq integer DEFAULT 0 NOT NULL,
+    created_user bigint DEFAULT 0 NOT NULL,
+    update_user bigint DEFAULT 0 NOT NULL,
+    created_at timestamp DEFAULT now() NOT NULL,
+    updated_at timestamp DEFAULT now() NOT NULL,
+    PRIMARY KEY (id)
+);
+
+COMMENT ON TABLE chestnut_flow_node_instance IS '流程节点实例表';
+COMMENT ON COLUMN chestnut_flow_node_instance.id IS 'ID';
+COMMENT ON COLUMN chestnut_flow_node_instance.node_id IS '对应节点id';
+COMMENT ON COLUMN chestnut_flow_node_instance.flow_instance_id IS '对应流程实例id';
+COMMENT ON COLUMN chestnut_flow_node_instance.status IS '节点实例状态';
+COMMENT ON COLUMN chestnut_flow_node_instance.seq IS '节点在流程实例中的递增序号';
+COMMENT ON COLUMN chestnut_flow_node_instance.created_user IS '创建者';
+COMMENT ON COLUMN chestnut_flow_node_instance.update_user IS '更新者';
+COMMENT ON COLUMN chestnut_flow_node_instance.created_at IS '创建时间';
+COMMENT ON COLUMN chestnut_flow_node_instance.updated_at IS '更新时间';
