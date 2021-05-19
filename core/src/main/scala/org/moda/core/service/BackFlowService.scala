bb90fcafa6df820f1a7f60daa73c13ffb8a2dac1
yangxuan_321 <yangxuan_321@163.com>
2021-05-19 23:25:06 +0800

    引入前端框架

--- core/src/main/scala/org/moda/core/service/BackFlowService.scala
+++ core/src/main/scala/org/moda/core/service/BackFlowService.scala
@@ -1,24 +1,50 @@
 package org.moda.core.service
 
+import java.io.File
+
 import com.typesafe.scalalogging.Logger
+import io.circe.generic.auto._
+import io.circe.parser._
 import org.moda.common.database.DatabaseComponent
 import reactivemongo.api.DB
-import reactivemongo.api.bson.BSONDocument
-import reactivemongo.api.bson.collection.BSONCollection
-import reactivemongo.api.commands.WriteResult
 
 import scala.concurrent.Future
+import scala.io.Source
 
 object BackFlowService {
   def apply()(implicit dc: DatabaseComponent, mongo: DB): MongoService = new MongoService()
 }
 
 class BackFlowService(implicit dc: DatabaseComponent, mongo: DB) {
-  import scala.concurrent.ExecutionContext.Implicits.global
   val logger: Logger = Logger(getClass)
+  import scala.concurrent.ExecutionContext.Implicits.global
 
-  def createNode(): Future[Int] = {
+  def typeGroups(): Future[Vector[TypeGroup]] = {
+    val c = assembleTypeGroupData()
+    val x = decode[Vector[TypeGroup]](c) match {
+      case Left(l) => Vector.empty[TypeGroup]
+      case Right(r) => r
+    }
 
+    Future {x}
+  }
+
+  def assembleTypeGroupData(): String = {
+    val f = Source.fromFile(new File("/data/TypeGroupData.json"))
+    val c: String = f.getLines().toList.foldLeft("")((r, e) => r + e)
+    f.close()
+    c
   }
 }
+
+case class TypeGroup(id: String, title: String, name: String, items: Vector[TypeItem])
+case class TypeItem(
+  id: String,
+  name: String,
+  icon: String,
+  iconOnRight: Boolean,
+  color: String,
+  hasInput: Boolean,
+  hasOutput: Boolean
+)
 
