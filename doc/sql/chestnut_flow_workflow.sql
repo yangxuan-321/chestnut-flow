c235309184ff08cf1f4d53fe2f720722a564cedb
yangxuan_321 <yangxuan_321@163.com>
2021-07-21 12:58:45 +0800

    启动一个流程的逻辑编写

--- doc/sql/chestnut_flow_workflow.sql
+++ doc/sql/chestnut_flow_workflow.sql
@@ -1,5 +1,6 @@
 CREATE TABLE chestnut_flow_workflow(
     id bigserial NOT NULL,
+    flow_uuid text DEFAULT '' NOT NULL,
     flow_name text DEFAULT '' NOT NULL,
     template_id bigint DEFAULT 0 NOT NULL,
     flow_version text DEFAULT '' NOT NULL,
@@ -17,10 +18,11 @@ CREATE UNIQUE INDEX idx_chestnut_flow_workflow ON chestnut_flow_workflow(templat
 
 COMMENT ON TABLE chestnut_flow_workflow IS '流程表';
 COMMENT ON COLUMN chestnut_flow_workflow.id IS 'ID';
+COMMENT ON COLUMN chestnut_flow_workflow.flow_uuid IS '流程UUID';
 COMMENT ON COLUMN chestnut_flow_workflow.flow_name IS '流程名称';
 COMMENT ON COLUMN chestnut_flow_workflow.template_id IS '对应的 模板Id 纵然版本不同 模板Id用来标识 是同一个模板';
 COMMENT ON COLUMN chestnut_flow_workflow.flow_version IS '流程版本';
-COMMENT ON COLUMN chestnut_flow_workflow.version IS '流程版本中的小版本-每次保存+1';
+COMMENT ON COLUMN chestnut_flow_workflow.version IS '流程版本-小版本';
 COMMENT ON COLUMN chestnut_flow_workflow.status IS '流程状态';
 COMMENT ON COLUMN chestnut_flow_workflow.description IS '流程备注描述';
 COMMENT ON COLUMN chestnut_flow_workflow.create_user IS '创建者';
